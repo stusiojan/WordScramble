17d479aae755bb172777016eb98539336d1f6c2f
Jan Stusio <111381938+stusiojan@users.noreply.github.com>
2023-03-22 21:45:39 +0100

    Add files via upload

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Jan Stusio on 01/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
